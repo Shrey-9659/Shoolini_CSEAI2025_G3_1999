59dd2f6935dbbf8d620fa18cc5e8d413373bdc4c
Shrey Khandelwal <shrey.khandelwal.2000@gmail.com>
2025-11-24 11:47:18 +0530

    DOM lecture

---
+++ JS/DOM/script.js
@@ -0,0 +1,18 @@
+// console.log("I am running...")
+
+// 1. Selecting an element
+// console.log(document.getElementById("heading"))
+let heading = document.getElementById("heading")
+// console.log(heading)
+
+// console.log(document.getElementsByClassName("btn"))
+
+// console.log(document.querySelector("#heading"))
+// console.log(document.querySelectorAll(".btn"))
+// console.log(document.querySelector("p"))
+
+// 2. Getting the value -- .innerText
+console.log(heading.innerText)
+// 3. Setting the value
+heading.innerText = "Welcome to Shoolini University"
+// 4. Event Listeners
---
+++ JS/G5/DOM/script.js
@@ -0,0 +1,31 @@
+// DOM : Document Object Model
+// console.log("first")
+
+// 1. Selecting an element
+// getElementById, getElementByClassName, getElementByTagName
+// querySelector, querySelectorAll
+// let h1 = document.getElementById("heading")
+// console.log(document.getElementsByTagName("h1"))
+// console.log(h1)
+// let h1ById = document.querySelector("#heading");
+// let h1ByClass = document.querySelector(".group");
+// let h1ByTagUsingQS = document.querySelector("h1");
+// let h1ByTagUsingQSA = document.querySelectorAll("h1");
+// console.log(h1ByTagUsingQSA)
+
+// 2. Getting the value of an element
+// innerText
+// let h1 = document.getElementById("heading")
+// console.log(h1.innerText)
+
+// 3. Setting the value of an element
+// console.log(h1.innerText)
+// h1.innerText = "Hello World From Shoolini University"
+// 4. Event Listeners
+
+// let btn = document.querySelector(".btn")
+// let h1 = document.getElementById("heading")
+
+// btn.addEventListener("click", function(){
+//     h1.innerText = "Button is clicked, hello from Shoolini"
+// })
