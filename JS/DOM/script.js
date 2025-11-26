ebaeae30ca879f25c994498de5eec4929f22cdf9
Shrey Khandelwal <shrey.khandelwal.2000@gmail.com>
2025-11-26 10:44:53 +0530

    value in G6P

--- JS/DOM/script.js
+++ JS/DOM/script.js
@@ -2,7 +2,7 @@
 
 // 1. Selecting an element
 // console.log(document.getElementById("heading"))
-let heading = document.getElementById("heading")
+// let heading = document.getElementById("heading")
 // console.log(heading)
 
 // console.log(document.getElementsByClassName("btn"))
@@ -12,7 +12,25 @@ let heading = document.getElementById("heading")
 // console.log(document.querySelector("p"))
 
 // 2. Getting the value -- .innerText
-console.log(heading.innerText)
+// console.log(heading.innerText)
 // 3. Setting the value
-heading.innerText = "Welcome to Shoolini University"
+// heading.innerText = "Welcome to Shoolini University"
 // 4. Event Listeners
+
+let eventBtn = document.getElementById("eventBtn")
+let heading = document.getElementById("heading")
+let userName = document.getElementById("userName")
+// eventBtn.addEventListener("click", function(){
+//     // console.log("Button clicked")
+//     heading.innerText = "Welcome to my website, User"
+// })
+
+// reading input value
+eventBtn.addEventListener("click", function(){
+    if(userName.value == ""){
+        heading.innerText = "Welcome to my website, User"
+    }else{
+        heading.innerText = "Welcome to my website, " + userName.value
+        userName.value = ""
+    }
+})
